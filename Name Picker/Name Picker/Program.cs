d8c6b6aeda5a5662c32f59a92923dc5b68385b28
Logan Heath <100187341+JustAHubber@users.noreply.github.com>
2024-02-27 22:26:17 +0000

    Add files via upload

---
+++ Name Picker/Name Picker/Program.cs
@@ -0,0 +1,60 @@
+﻿String userOk = "N";
+String[] nuhUhWords = { "Funk", "Shot", "Drat", "Flip", "Poo", "Damn", "Leah", "Sheila", "Ball"};
+
+// Name generator
+String generateName()
+{
+    String[] firstNames = { "Logan", "David", "James", "Jacob", "Alistair", "Issa", "Leah", "James", "Jashandeep", "Ethan"};
+    String[] lastNames = { "Heath", "Day", "Theophilou", "Harrod", "Howles", "Abdu", "Ball", "Baker", "Singh", "Conolly"};
+
+    Random dice = new Random();
+
+    String temp = firstNames[dice.Next(0, firstNames.Length)] + "" + lastNames[dice.Next(0, lastNames.Length)];
+    return temp;
+}
+
+String Multiverse()
+{
+    String userName = generateName();
+    Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Your Username is: " + userName);
+    return userName;
+}
+
+void Megaverse()
+{
+    String userName = Multiverse();
+    Console.ForegroundColor = ConsoleColor.Yellow; Console.Write("Are you happy with your name (Y/N): "); userOk = Console.ReadLine().ToUpper();
+
+    if (userOk.Equals("Y"))
+    {
+        Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Welcome to the game " + userName);
+    }
+    else if (userOk.Equals("N"))
+    {
+        Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Generating new name...");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Incorrect Input, Keeping Name!");
+        Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Welcome to the game " + userName);
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.Blue; Console.WriteLine("Welcome to the Kahurt Name Generator");
+Console.Write("Please enter your Username: "); String userName = Console.ReadLine();
+
+if (nuhUhWords.Any(word => userName.Contains(word)))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("NUH UH, YOU CAN'T SAY THAT. BANISHED!");
+    while (userOk.Equals("N"))
+    {
+        Megaverse();
+    }
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Welcome to the game: " + userName);
+}
+Console.ForegroundColor = ConsoleColor.White;
---
+++ Name Picker/Program.cs
@@ -0,0 +1,51 @@
+﻿// Name generater
+String generateName()
+{
+    String[] firstNames = { "Logan", "David", "James", "Jacob", "Alistair", "Issa" };
+    String[] lastNames = { "Heath", "Day", "Theophilou", "Harrod", "Howles", "Abdu" };
+
+    Random dice = new Random();
+
+    String temp = firstNames[dice.Next(0, 6)] + "" + lastNames[dice.Next(0, 6)];
+    return temp;
+}
+
+String Multiverse()
+{
+    String userName = generateName();
+    Console.WriteLine("Your Username is: " + userName);
+    return userName;
+}
+
+bool Megaverse()
+{
+    bool again = true;
+
+    String userName = Multiverse();
+    Console.Write("Are you happy with your name (Y/N)?"); String userOk = Console.ReadLine();
+
+    if (userOk.Equals("Y", StringComparison.CurrentCultureIgnoreCase))
+    {
+        Console.WriteLine("Welcome to the game " + userName);
+        again = false;
+    }
+    else if (userOk.Equals("N", StringComparison.CurrentCultureIgnoreCase))
+    {
+        Console.WriteLine("Generating new name...");
+        again = true;
+    }
+    else
+    {
+        Console.WriteLine("Incorrect Input, Keeping Name!");
+        Console.WriteLine("Welcome to the game " + userName);
+        again = false;
+    }
+    return again;
+}
+
+bool again = true;
+
+while (again = true) 
+{
+    Megaverse();
+}
